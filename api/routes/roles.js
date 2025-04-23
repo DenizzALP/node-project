c1fe0f9330ee0acec0c860c4f57da3752837f8de
Erda Deniz ALP <erdadenizalp@hotmail.com>
2025-04-23 17:20:47 +0300

    #5 role mekanizmasi eklendi

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -8,9 +8,14 @@ const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
 const role_privileges= require("../config/role_privileges");
 // const RolePrivileges = require("../db/models/RolePrivileges");
+const auth = require("../lib/auth")()
+
+router.all("*",auth.authenticate(), (req, res, next)=>{
+  next();
+});
 
 
-router.get("/", async(req, res)=>{
+router.get("/",auth.checkRoles("role_view"), async(req, res)=>{
     try{
         let roles = await Roles.find({});
 
@@ -23,7 +28,7 @@ router.get("/", async(req, res)=>{
     }
 })
 
-router.post("/add", async (req, res)=>{
+router.post("/add",auth.checkRoles("role_add"), async (req, res)=>{
     let body = req.body;
 
     try{
@@ -59,7 +64,7 @@ router.post("/add", async (req, res)=>{
     }
 })
 
-router.post("/update", async (req, res)=>{
+router.post("/update",auth.checkRoles("role_update"), async (req, res)=>{
     let body = req.body;
 
     try{
@@ -110,7 +115,7 @@ router.post("/update", async (req, res)=>{
 })
 
 
-router.post("/delete", async (req, res)=>{
+router.post("/delete",auth.checkRoles("role_delete"), async (req, res)=>{
     let body = req.body;
 
     try{
