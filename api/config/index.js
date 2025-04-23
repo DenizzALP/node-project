bc97b360fd23b1af720293179b016f7294d3c731
Erda Deniz ALP <erdadenizalp@hotmail.com>
2025-04-23 21:18:23 +0300

    #8 coklu dil secenegi eklendi

--- api/config/index.js
+++ api/config/index.js
@@ -7,6 +7,7 @@ module.exports ={//bu dosyanin import edilebilir oldugunu belirtir
     "JWT":{
         "SECRET": "123456",
         "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24*60*60 // 86400
-    }
+    },
+    "DEFAULT_LANG": process.env.DEFAULT_LANG || "EN"
 
 }
---
+++ api/i18n/tr.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "COMMON":{
+        "VALIDATION_ERROR_TITLE": "Doğrulama Hatası",
+        "ALREADY_EXXIST":"Zaten var!",
+        "UNKNOWN_ERROR": "Bilinmeyen Hata!",
+        "FIELD_MUST_BE_FILLED": "{} alanı dolu olmalıdır ",
+        "FIELD_MUST_BE_TYPE": "{} alanı {} tipinde olmalıdır"
+    },
+    "USERS":{
+        "AUTH_ERROR": "Email ya da  şifre hatalı",
+        "EMAIL_FORMAT_ERROR": "email alanı doğru bir email formatında olmalıdır",
+        "PASSWORD_LENGTH_ERROR": "paswword uzunluğu en az {} olmalıdır"
+    }
+
+}
