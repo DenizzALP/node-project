e17429a4bf3c9772875188b1ce2026397a48961e
Erda Deniz ALP <erdadenizalp@hotmail.com>
2025-04-25 15:27:03 +0300

    #4 Excel import

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -11,7 +11,21 @@ const auth = require("../lib/auth")()
 const i18n = new (require("../lib/i18n"))(config.DEFAULT_LANG);
 const emitter = require("../lib/Emitter");
 const excelExport = new (require("../lib/Export"))();
-const fs = require("fs")
+const fs = require("fs");
+const multer = require("multer");
+const path = require('path');
+const Import =  new (require("../lib/Import"))
+
+let multerStorage = multer.diskStorage({
+  destination:(req, file, next) =>{
+    next(null, config.FILE_UPLOAD_PATH)
+  },
+  filename:(req,file,next)=>{
+    next(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({storage: multerStorage}).single("pb_file")
 
 router.all("*",auth.authenticate(), (req, res, next)=>{
   next();
@@ -122,6 +136,34 @@ router.post("/export", auth.checkRoles("category_export"), async (req,res) =>{
     res.download(filePath);
 
     // fs.unlinkSync(filePath);
+  }catch(err){
+    let errorResponse = Response.errorResponse(err)
+    res.status(errorResponse.code).json(errorResponse)
+  }
+})
+
+router.post("/import", auth.checkRoles("category_add"), upload,async (req, res) =>{
+  try{
+
+    let file = req.file;
+    let body = req.body;
+
+    let rows = Import.fromExcel(file.path)
+
+    for (let i=1;i<rows.length;i++){
+      let [name, is_active, user, created_at, updaated_at] = rows[i]
+
+      if(name){
+        await Categories.create({
+          name,
+          is_active,
+          created_by: req.user._id
+        });
+      }
+    }
+
+    res.json(Response.succesResponse(req.body, Enum.HTTP_CODES.CREATED));
+
   }catch(err){
     let errorResponse = Response.errorResponse(err)
     res.status(errorResponse.code).json(errorResponse)
